674629bb4aaea876c0ddba9c11f2c65ae82546ba
woodylucas <wlucas0311@gmail.com>
2019-02-19 20:38:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+
+var dodger = document.getElementById('dodger')
+
+
+dodger.style.backgroundColor = "#000000";
+
+dodger.style.backgroundColor = '#FF69B4'
+
+dodger.style.left = "180px"
+
+dodger.style.bottom = '0px'
+
+
+
+function moveDodgerRight() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
+  }
+}
+
+
+
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`
+  }
+}
+
+
+
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    dodger.style.left = `${left - 1}px`
+  }
+})
+
+document.addEventListener('keydown', function(key) {
+  if (key.which === 39) {
+    moveDodgerRight()
+  }
+})
