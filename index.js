2616a09448a3c99ad7294df8a056af01af96fedb
tarasabella <tarasabella@gmail.com>
2019-02-21 11:05:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+
+var dodger = document.getElementById('dodger')
+// console.log(dodger)
+// dodger.style.backgroundColor = "#000000"
+dodger.style.backgroundColor = '#FF69B4'
+
+// dodger.style.bottom = '100px'
+dodger.style.bottom = '0px'
+
+document.addEventListener("keydown", (e) => {
+    console.log(e.which === 37) {
+      var leftNumbers = dodger.style.left.replace('px', '')
+      var left = parseInt(leftNumbers, 10)
+
+      dodger.style.left = `${left - 1}px`
+    }
+
+})
